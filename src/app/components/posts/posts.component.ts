6bf669d420750dc09db3b7368e17be11c551a95b
Hector <rootdevelopper>
2018-09-05 03:36:24 -0500

    Added http to post, post interface, and post form

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import {Post} from '../../models/Post';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts: Post[];
+  constructor(private postService: PostService ) { }
 
   ngOnInit() {
+    this.postService.getPosts().subscribe(posts => {
+      this.posts = posts;
+    });
   }
 
 }
